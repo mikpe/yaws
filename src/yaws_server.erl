d762dff99727d7178edf6bd1e51653786612299a
Uwe Dauernheim <uwe@klarna.com>
2012-07-05 19:41:00 +0200

    Pass stacktrace to errormod as well

--- src/yaws_server.erl
+++ src/yaws_server.erl
@@ -1961,12 +1961,13 @@ handle_auth(ARG, _Auth_H, Auth_methods=#auth{users=[],pam=false,mod=[]}, Ret) ->
 handle_auth(ARG, Auth_H, Auth_methods = #auth{mod = Mod}, Ret) when Mod /= [] ->
     case catch Mod:auth(ARG, Auth_methods) of
         {'EXIT', Reason} ->
+            ST = erlang:get_stacktrace(),
             L = ?F("authmod crashed ~n~p:auth(~p, ~n ~p) \n"
                    "Reason: ~p~n"
                    "Stack: ~p~n",
                    [Mod, ARG, Auth_methods, Reason,
-                    erlang:get_stacktrace()]),
-            handle_crash(ARG, L),
+                    ST]),
+            handle_crash(ARG, L, ST),
             deliver_accumulated(ARG#arg.clisock),
             exit(normal);
 
@@ -3261,12 +3262,13 @@ handle_out_reply(ok, _LineNo, _YawsFile, _UT, _ARG) ->
     ok;
 
 handle_out_reply({'EXIT', Err}, LineNo, YawsFile, _UT, ARG) ->
+    ST = erlang:get_stacktrace(),
     L = ?F("~n~nERROR erlang  code  crashed:~n "
            "File: ~s:~w~n"
            "Reason: ~p~nReq: ~p~n"
            "Stack: ~p~n",
-           [YawsFile, LineNo, Err, ARG#arg.req, erlang:get_stacktrace()]),
-    handle_crash(ARG, L);
+           [YawsFile, LineNo, Err, ARG#arg.req, ST]),
+    handle_crash(ARG, L, ST);
 
 handle_out_reply({get_more, Cont, State}, _LineNo, _YawsFile, _UT, _ARG) ->
     {get_more, Cont, State};
@@ -3458,6 +3460,11 @@ delim_split(_,_,[],Ack,DAcc) ->
 
 %% Erlang yaws code crashed, display either the
 %% actual crash or a customized error message
+
+%% Allow to pass the log string as well as a stacktrace.
+%% This patch has to be supported by the errormod, otherwise it might crash.
+handle_crash(ARG, L, ST) ->
+  handle_crash(ARG, {L, ST}).
 
 handle_crash(ARG, L) ->
     ?Debug("handle_crash(~p)~n", [L]),
